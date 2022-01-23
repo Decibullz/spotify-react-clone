e3d9f022d5eff14b8a7fceb0557bfd23f8f27437
Cody Snell <codyjsnell@yahoo.com>
2022-01-22 22:26:45 -0600

    adds token state, renders different views

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Login from './components/Login/Login'
+import { getToken } from './spotify'
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  )
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const hash = getToken()
+    // clears address bar removing access token from view
+    window.location.hash = ''
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+    }
+  }, [])
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>
 }
 
 export default App
