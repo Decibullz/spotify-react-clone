22f68d15cb74331f3ace80534fea27ed4ca5e627
Cody Snell <codyjsnell@yahoo.com>
2022-01-23 00:02:24 -0600

    connects app to data layer, set user action

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import SpotifyWebApi from 'spotify-web-api-js'
 import './App.css'
 import Login from './components/Login/Login'
 import Player from './components/Player/Player'
+import { useDataLayerValue } from './DataLayer'
 import { getToken } from './spotify'
 
 const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken] = useState(null)
+  const [{ user }, dispatch] = useDataLayerValue()
 
   useEffect(() => {
     const hash = getToken()
@@ -18,16 +20,21 @@ function App() {
 
     if (_token) {
       setToken(_token)
+
       // allows react to talk to spotify api
       spotify.setAccessToken(_token)
 
       // gets user info
       spotify.getMe().then((user) => {
-        console.log(user)
+        dispatch({
+          type: 'SET_USER',
+          user,
+        })
       })
     }
   }, [])
 
+  console.log(user)
   return <div className="app">{token ? <Player /> : <Login />}</div>
 }
 
