c3185d0aeda6c4d3a048bd438a2d015394287bdd
Cody Snell <codyjsnell@yahoo.com>
2022-01-22 21:53:59 -0600

    creates spotify endpoint URL

---
+++ src/spotify.js
@@ -0,0 +1,15 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize'
+const redirectUri = 'http://localhost:3000'
+const clientId = 'ba58c023d24e4da1a620be7d2bf62384'
+
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+]
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  '%20'
+)}&response_type=token&show_dialog=true`
