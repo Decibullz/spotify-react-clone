41a8ca88809a49bcafde16fbf355ad1dfba80797
Cody Snell <codyjsnell@yahoo.com>
2022-01-24 00:03:09 -0600

    removes unused var, error handling

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useDataLayerValue } from '../../DataLayer'
 import Body from '../Body/Body'
 import Footer from '../Footer/Footer'
 import Sidebar from '../Sidebar/Sidebar'
 import './Player.css'
 
 function Player() {
-  const [{ spotify }] = useDataLayerValue()
   return (
     <div className="player">
       <div className="player_body">
