aa25e647ed622c428cd777ae9d9b37f3f49fe764
Cody Snell <codyjsnell@yahoo.com>
2022-01-23 18:37:37 -0600

    create files, stub up components, adds stlying

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import './Footer.css'
+
+function Footer() {
+  return <div className="footer">Im the Footer</div>
+}
+
+export default Footer
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useDataLayerValue } from '../../DataLayer'
+import Body from '../Body/Body'
+import Footer from '../Footer/Footer'
+import Sidebar from '../Sidebar/Sidebar'
+import './Player.css'
 
 function Player() {
-  return <div>Welcome to Spotify</div>
+  const [{ spotify }] = useDataLayerValue()
+  return (
+    <div className="player">
+      <div className="player_body">
+        <Sidebar />
+
+        <Body />
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default Player
