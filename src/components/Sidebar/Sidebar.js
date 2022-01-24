240dab42a7bf55b14b654545103a68f5d46c78f2
Cody Snell <codyjsnell@yahoo.com>
2022-01-23 20:18:00 -0600

    renders playlists mapped from state, adds icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,28 @@
+import SidebarOptions from '../SidebarOptions/SidebarOptions'
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined'
+import SearchIcon from '@material-ui/icons/Search'
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic'
+
 import './Sidebar.css'
+import { useDataLayerValue } from '../../DataLayer'
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue()
   return (
     <div className="sidebar">
-      <h1>im a sidebar</h1>
+      <img
+        className="sidebar_logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebarOptions Icon={HomeOutlinedIcon} option="Home" />
+      <SidebarOptions Icon={SearchIcon} option="Search" />
+      <SidebarOptions Icon={LibraryMusicIcon} option="Your Library" />
+      <br />
+      {/* <strong className="sidebar_title">PLAYLISTS</strong> */}
+      <hr />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOptions option={playlist.name} />
+      ))}
     </div>
   )
 }
