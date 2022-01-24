22989c610605f966faafcfd1ea3be2f6e7a673fd
Cody Snell <codyjsnell@yahoo.com>
2022-01-24 00:02:53 -0600

    displays discover weekly header data from api

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,21 @@
+import { useDataLayerValue } from '../../DataLayer'
 import Header from '../Header/Header'
 import './Body.css'
 
 function Body() {
+  const [{ discoverWeekly }] = useDataLayerValue()
   return (
     <div className="body">
       <Header />
+      <div className="body_info">
+        <img src={discoverWeekly?.images[0]?.url} alt="" />
+        <div className="body_infoText">
+          <strong>{discoverWeekly?.type}</strong>
+          <h2>{discoverWeekly?.name}</h2>
+          <p>{discoverWeekly?.description}</p>
+        </div>
+      </div>
+      <div className="body_songs"></div>
     </div>
   )
 }
